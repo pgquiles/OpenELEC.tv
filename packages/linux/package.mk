c507834d069fa27bcc7de8faa22da07698705981
alex <surkov.alex@gmail.com>
2015-07-25 13:33:26 +0300

    [aml] update kernel version

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -19,7 +19,7 @@
 PKG_NAME="linux"
 case "$LINUX" in
   amlogic)
-    LINUX_VERSION="7963108"
+    LINUX_VERSION="3.10-cdfc6ee"
     PKG_VERSION="amlogic-$LINUX_VERSION"
     PKG_URL="http://amlinux.ru/source/$PKG_NAME-$PKG_VERSION.tar.gz"
     ;;
